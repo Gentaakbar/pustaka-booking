8dcd17ec2a18e20962aa0ec6bc5e05dc8b0cd9cc
Gentaakbar <gentalol39@gmail.com>
2024-05-28 16:02:00 +0700

    commit

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -26,8 +26,10 @@ class User extends CI_Controller
         $data['judul'] = 'Data Anggota';
         $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
 
-        $this->db->where('role_id', 1);
-        $data['anggota'] = $this->db->get('user')->result_array();
+        // $this->db->where('role_id', 1);
+        // $data['anggota'] = $this->db->get('user')->result_array();
+        $roleid=['role_id' => 2];
+        $data['anggota'] = $this->ModelUser->getuserwhere($roleid)->result();
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
