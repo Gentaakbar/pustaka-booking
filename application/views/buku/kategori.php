e90c92ab46ff578a1b1b631c168a8a27f660154d
yuma514 <falyaumairfansyah00@gmail.com>
2024-06-11 15:33:13 +0700

    "Updated Buku controller, added ubahBuku and ubahKategori methods, modified kategori method, removed hapusKategori method, updated kategori view, and deleted index.html file from libraries directory."

--- application/views/buku/kategori.php
+++ application/views/buku/kategori.php
@@ -9,8 +9,9 @@
                 </div>
             <?php } ?>
             <?= $this->session->flashdata('pesan'); ?>
-            <a href="" class="btn btn-primary mb-3" datatoggle="modal" data-target="#kategoriBaruModal"><i
-                    class="fas fa-file-alt"></i> Tambah Kategori</a>
+            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#kategoriBaruModal">
+                <i class="fas fa-file-alt"></i> Tambah Kategori
+            </a>
             <table class="table table-hover">
                 <thead>
                     <tr>
@@ -20,18 +21,14 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php
-                    $a = 1;
+                    <?php $a = 1;
                     foreach ($kategori as $k) { ?>
                         <tr>
                             <th scope="row"><?= $a++; ?></th>
                             <td><?= $k['kategori']; ?></td>
                             <td>
-                                <a href="<?=
-                                    base_url('buku/ubahBuku/') . $k['id']; ?>" class="badge badge-info"><i class="fas fa-edit"></i> Ubah</a>
-                                <a href="<?=
-                                    base_url('buku/hapusbuku/') . $k['id']; ?>" onclick="returnconfirm('Kamu yakin akan menghapus <?= $judul . ' ' . $k['kategori']; ?>?');" class="badge badge-danger"><i class="fas fa-trash"></i>
-                                    Hapus</a>
+                                <a href="<?= base_url('buku/ubahkategori/') . $k['id']; ?>" class="badge badge-info"><i class="fas fa-edit"></i> Ubah</a>
+                                <a href="<?= base_url('buku/hapusKategori/') . $k['id']; ?>" onclick="return confirm('Kamu yakin akan menghapus <?= $judul . ' ' . $k['kategori']; ?>?');" class="badge badge-danger"><i class="fas fa-trash"></i> Hapus</a>
                             </td>
                         </tr>
                     <?php } ?>
@@ -43,9 +40,9 @@
 <!-- /.container-fluid -->
 </div>
 <!-- End of Main Content -->
+
 <!-- Modal Tambah kategori baru-->
-<div class="modal fade" id="kategoriBaruModal" tabindex="-1" role="dialog" aria-labelledby="kategoriBaruModalLabel"
-    aria-hidden="true">
+<div class="modal fade" id="kategoriBaruModal" tabindex="-1" role="dialog" aria-labelledby="kategoriBaruModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -57,37 +54,15 @@
             <form action="<?= base_url('buku/kategori'); ?>" method="post">
                 <div class="modal-body">
                     <div class="form-group">
-                        <select name="kategori" class="form-control form-control-user">
-                            <option value="">Pilih Kategori</option>
-                            <?php
-                            $k =
-                                [
-                                    'Sains',
-                                    'Hobby',
-                                    'Komputer',
-                                    'Komunikasi',
-                                    'Hukum',
-                                    'Agama',
-                                    'Populer', 
-                                    'Bahasa',
-                                    'Komik'
-                                ];
-                            for ($i = 0; $i < 9; $i++) { ?>
-                                <option value="<?= $k[$i]; ?>">
-                                    <?=
-                                        $k[$i]; ?>
-                                </option>
-                            <?php } ?>
-                        </select>
+                        <input type="text" name="kategori" id="kategori" placeholder="Masukan Nama Kategori" class="form-control form-control-user"></input>
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-ban"></i>
-                        Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-ban"></i> Close</button>
                     <button type="submit" class="btn btn-primary"><i class="fas fa-plus-circle"></i> Tambah</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
-<!-- End of Modal Tambah Menu -->
+<!-- End of Modal Tambah Kategori -->
